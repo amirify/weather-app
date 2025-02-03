b2830397af9c16f2a56db46e610d972968c066e3
Amir H. Mohammadi <25364126+amirify@users.noreply.github.com>
2025-02-03 18:09:27 +0330

    add unmount functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Create a root element for rendering the app
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// Render the app initially
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// Call this function to unmount the app
+window.unmountWeatherApp = () => {
+  root.unmount();
+};
